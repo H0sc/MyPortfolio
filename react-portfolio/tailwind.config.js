830a09eab2061f367a021e469fd0dcd1ae361b39
H0sc <timon.hosch@gmail.com>
2023-02-07 21:35:49 +0100

    progress halted at module related error

---
+++ react-portfolio/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./app/**/*.{js,ts,jsx,tsx}",
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+
+  // Or if using `src` directory:
+  "./src/**/*.{js,mjs,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+      burtons: "burtons"
+    }
+  }
+};
+export const plugins = [];
