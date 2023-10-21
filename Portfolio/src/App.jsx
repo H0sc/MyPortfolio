118510adc15c7063d4c579d68ce014839d130f28
Timon Hosch <timon.hosch@gmail.com>
2023-10-21 19:58:08 +0200

    installed dependencies. beginning bootstrapping

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav className="container">
+        <div className="row row-cols-4">
+          <div className="col">
+            <div className="box"></div>
+          </div>
+
+          <div className="col">
+            <div className="box"></div>
+          </div>
+
+          <div className="col">
+            <div className="box"></div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default App;
