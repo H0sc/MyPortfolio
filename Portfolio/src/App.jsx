70b787420d1dfed5f097342c448f8eb83fd04249
Timon Hosch <timon.hosch@gmail.com>
2023-11-04 19:13:35 +0100

    navbar added

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,19 +1,27 @@
-import "./App.scss"
-import Navbar from "react-bootstrap/Navbar"
-import Container from "react-bootstrap/Navbar"
+import { Nav, Navbar, Container } from "react-bootstrap";
+import "./index.scss"
 
 
 function App() {
   return (
     <>
-      <Navbar expand="lg">
+      <nav>
+              <Navbar bg="light" data-bs-theme="light" expand="sm">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home" className="me-auto">Home</Navbar.Brand>
+          <Nav className="me-0">
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
-      <div className="container bg-light shadow">
-        <p className="display-2"> I'm the container </p>
-      </div>
+      </nav>
+
+      <main>
+        <div className="box border mb-2"></div>
+        <div className="box border mb-2"></div>
+        <div className="box border"></div>
+      </main>
     </>
   );
 }
