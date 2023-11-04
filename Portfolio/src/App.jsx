d2dd945a6de15edeb576319a9a9c6509cbe900e1
Timon Hosch <timon.hosch@gmail.com>
2023-11-04 19:45:54 +0100

    first layout homepage done

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,27 +1,45 @@
 import { Nav, Navbar, Container } from "react-bootstrap";
-import "./index.scss"
-
+import "./index.scss";
 
 function App() {
   return (
     <>
       <nav>
-              <Navbar bg="light" data-bs-theme="light" expand="sm">
-        <Container>
-          <Navbar.Brand href="#home" className="me-auto">Home</Navbar.Brand>
-          <Nav className="me-0">
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+        <Navbar bg="light" data-bs-theme="light" expand="sm">
+          <Container>
+            <Navbar.Brand href="#home" className="me-auto">
+              Home
+            </Navbar.Brand>
+            <Nav className="me-0">
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
       </nav>
 
       <main>
-        <div className="box border mb-2"></div>
-        <div className="box border mb-2"></div>
-        <div className="box border"></div>
+        <header>
+          <div
+            className="border bg-secondary"
+            style={{ height: "500px" }}
+          ></div>
+          <div className="container-fluid p-0">
+            <div className="row">
+              <div
+                className="box border col"
+                style={{ height: "250px" }}
+              ></div>
+              <div
+                className="box border col"
+                style={{ height: "250px" }}
+              ></div>
+            </div>
+          </div>
+        </header>
       </main>
+      <footer className="bg-secondary" style={{height: "50px"}}>
+      </footer>
     </>
   );
 }
